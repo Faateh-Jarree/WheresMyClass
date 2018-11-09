75c832d392d176428224e5a3a97d384edbd359d7
Faateh Jarree <arronstn@gmail.com>
2018-11-09 21:59:03 +0500

    Second Commit by Jarree: Shiza Drawer working properly, but theme not fixed.

--- app/src/main/java/com/droids/ffs/smd_project/ViewSchedule.java
+++ app/src/main/java/com/droids/ffs/smd_project/ViewSchedule.java
@@ -49,15 +49,6 @@ public class ViewSchedule extends AppCompatActivity implements NavigationView.On
         actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
         actionbar.setTitle(null);
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
-
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
